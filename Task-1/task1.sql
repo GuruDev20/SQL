4559184c559bfa0a336f12ac9541ef34b041a8a6
GuruDev <guru01803@gmail.com>
2025-04-04 23:32:37 +0530

    Inserted values to employees table

--- Task-1/task1.sql
+++ Task-1/task1.sql
@@ -8,3 +8,11 @@ CREATE TABLE Employees (
     Salary DECIMAL(10, 2),
     HireDate DATE
 );
+
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Department, Salary, HireDate)
+VALUES 
+    (1, 'Guru', 'Dev', 'Engineering', 75000.00, '2022-03-15'),
+    (2, 'Divya', 'Dharshini', 'Marketing', 62000.00, '2021-11-22'),
+    (3, 'Hemanth', 'Kumar', 'HR', 58000.00, '2023-01-10'),
+    (4, 'Dhenu', 'DS', 'Engineering', 80000.00, '2020-07-19'),
+    (5, 'Ananya', 'Raj', 'Finance', 67000.00, '2022-06-01');
