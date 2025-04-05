fd13e27cd9827ee764bce97920b51b3d2537df14
GuruDev <guru01803@gmail.com>
2025-04-05 21:58:01 +0530

    Updated files with Basic Filtering and Sorting

--- Task-2/task2.sql
+++ Task-2/task2.sql
@@ -1,2 +1,11 @@
 -- Active: 1743788394603@@127.0.0.1@3306@sql_tasks
 
+SELECT * FROM Employees WHERE Department = 'Engineering';
+
+SELECT * FROM Employees ORDER BY LastName ASC;
+
+SELECT * FROM Employees WHERE Salary > 65000;
+
+SELECT * FROM Employees WHERE Department = 'Engineering' AND Salary > 76000;
+
+SELECT * FROM Employees ORDER BY Salary DESC;
