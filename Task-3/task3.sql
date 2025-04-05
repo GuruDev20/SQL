f2edf0d7ad5a6f9f422832293a95f2c9aac5904b
GuruDev <guru01803@gmail.com>
2025-04-05 22:02:15 +0530

    Updated files with Simple Aggregation and Grouping

--- Task-3/task3.sql
+++ Task-3/task3.sql
@@ -1,2 +1,13 @@
 -- Active: 1743788394603@@127.0.0.1@3306@sql_tasks
 
+SELECT COUNT(*) AS TotalEmployees FROM Employees;
+
+SELECT AVG(Salary) AS AverageSalary FROM Employees;
+
+SELECT Department, SUM(Salary) AS TotalDepartmentSalary FROM Employees GROUP BY Department;
+
+SELECT Department, COUNT(*) AS EmployeeCount FROM Employees GROUP BY Department;
+
+SELECT Department, AVG(Salary) AS AvgSalary FROM Employees GROUP BY Department;
+
+SELECT Department, COUNT(*) AS EmployeeCount FROM Employees GROUP BY Department HAVING COUNT(*) > 1;
