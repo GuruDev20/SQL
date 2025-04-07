de3256fe374713ca25b0b0542970212fb67af762
GuruDev <guru01803@gmail.com>
2025-04-07 21:54:54 +0530

    Added Transactions and views

--- Task-10/transactions.sql
+++ Task-10/transactions.sql
@@ -1,1 +1,14 @@
 -- Active: 1743788394603@@127.0.0.1@3306@sql_tasks
+
+START TRANSACTION;
+
+INSERT INTO Orders (user_id, order_date, status, total_amount)
+VALUES (1, CURDATE(), 'Processing', 300.00);
+
+INSERT INTO OrderDetails (order_id, product_id, quantity, price)
+VALUES (LAST_INSERT_ID(), 1, 2, 150.00);
+
+INSERT INTO Payments (order_id, amount, payment_method, payment_date)
+VALUES (LAST_INSERT_ID(), 300.00, 'Card', CURDATE());
+
+COMMIT;
