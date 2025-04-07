a1192f241cdd7dddd80016acc97eeb64ac2dfb51
GuruDev <guru01803@gmail.com>
2025-04-07 19:58:41 +0530

    Updated Files for Stored Procedures and User-Defined Functions

--- Task-9/task9.sql
+++ Task-9/task9.sql
@@ -1,2 +1,37 @@
 -- Active: 1743788394603@@127.0.0.1@3306@sql_tasks
 
+DELIMITER
+
+CREATE PROCEDURE GetOrdersInRange(IN startDate DATE, IN endDate DATE)
+BEGIN
+    SELECT * 
+    FROM Orders
+    WHERE OrderDate BETWEEN startDate AND endDate;
+END
+
+DELIMITER ;
+
+CALL GetOrdersInRange('2024-01-01', '2024-12-31');
+
+
+
+DELIMITER 
+
+CREATE FUNCTION CalculateBonus(salary DECIMAL(10,2))
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    DECLARE bonus DECIMAL(10,2);
+
+    IF salary > 70000 THEN
+        SET bonus = salary * 0.10;
+    ELSE
+        SET bonus = salary * 0.05;
+    END IF;
+
+    RETURN bonus;
+END 
+
+DELIMITER ;
+
+SELECT FirstName,LastName,Salary,CalculateBonus(Salary) AS Bonus FROM Employees;
