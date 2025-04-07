3ce7c6b8b4ebf725afc2a20f9e7d4f487e33fcbe
GuruDev <guru01803@gmail.com>
2025-04-07 19:37:28 +0530

    Updated Files for Common Table Expressions (CTEs) and Recursive Queries

--- Task-8/task8.sql
+++ Task-8/task8.sql
@@ -1,2 +1,32 @@
 -- Active: 1743788394603@@127.0.0.1@3306@sql_tasks
 
+WITH DeptAvg AS (
+    SELECT Department, AVG(Salary) AS AvgSalary
+    FROM Employees
+    GROUP BY Department
+)
+SELECT * FROM DeptAvg WHERE AvgSalary > 70000;
+
+WITH RECURSIVE EmployeeHierarchy AS (
+    SELECT 
+        EmployeeID,
+        Name,
+        ManagerID,
+        1 AS Level
+    FROM OrgChart
+    WHERE ManagerID IS NULL
+
+    UNION ALL
+
+    SELECT 
+        E.EmployeeID,
+        E.Name,
+        E.ManagerID,
+        EH.Level + 1
+    FROM OrgChart E
+    INNER JOIN EmployeeHierarchy EH ON E.ManagerID = EH.EmployeeID
+)
+
+SELECT * 
+FROM EmployeeHierarchy
+ORDER BY Level, EmployeeID
